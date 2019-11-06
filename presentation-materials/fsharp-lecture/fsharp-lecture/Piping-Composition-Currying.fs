ae932f00e87dccbb8b1e239550bd89757e8a5565
Tyler Berkshire <berkshiret1@udayton.edu_>
2019-11-05 23:44:20 -0500

    Finished presentation materials

--- presentation-materials/fsharp-lecture/fsharp-lecture/Piping-Composition-Currying.fs
+++ presentation-materials/fsharp-lecture/fsharp-lecture/Piping-Composition-Currying.fs
@@ -1,35 +1,39 @@
 ﻿module Piping_etc
 
-(* Piping etc
-let main() =
-    let list = [1 .. 10 ] |> Seq.filter (fun x -> x % 2 <> 0)
+let run() = 
+    // Piping
+    let squareOddsPipe = [1 .. 10 ] |> Seq.filter (fun x -> x % 2 <> 0)
                           |> Seq.map (fun x -> x * x)
                           |> Seq.sum
-    printfn "%A" list
+    printfn "%A" squareOddsPipe
 
-    let composed = Seq.filter (fun x -> x % 2 <> 0)
-                   >> Seq.map (fun x -> x * x)
-                   >> Seq.sum
-    printfn "%A" (composed [1 .. 10])
-    //printfn "%A" <| composed [1 .. 10]
+    // Composition
+    let squareOddsComp = Seq.filter (fun x -> x % 2 <> 0)
+                        >> Seq.map (fun x -> x * x)
+                        >> Seq.sum
+    printfn "%A" (squareOddsComp [1 .. 10])
+    //printfn "%A" <| squareOddsComp [1 .. 10]
+    
+    // foo (fun x -> x |> bar |> baz)
+    // foo (bar >> baz)
 
-    let printTwo a b = printfn "%A and %A" a b
-    printTwo "CPS" 452
+    // Currying
+    let printTwo(a,b) = printfn "%A and %A" a b
+//    printTwo("CPS", 452)
 
     let printTwoCurried a =
         let printTwoSecond b =
             printfn "%A and %A" a b
         printTwoSecond
-    printTwoCurried "CPS" 452
+//    printTwoCurried "CPS" 452
 
-    let filterEvens = List.filter (fun x -> x % 2 = 0)
-    printfn "%A" <| filterEvens [ 1 .. 5 ] 
+    let filterEvens = List.filter (fun x -> x % 2 = 0)      // partial application
+//    printfn "%A" <| filterEvens [ 1 .. 10 ] 
 
-    let replace oldStr newStr (s:String) = s.Replace(oldValue = oldStr, newValue = newStr)   // Input string is now the last parameter
-    let result =
-        "CPS"
-        |> replace "CPS" "452"
-    printfn "%A" result
+    let replace oldStr newStr (s:string) = s.Replace(oldValue = oldStr, newValue = newStr)   // Rewrite .NET library func for partial app
+    let result x =
+        x
+        |> replace "CPS" "452"                              // input string is now the last param
+//    printfn "%A" (result "CPS 452 is the best CPS class")
     
-main()
-*)
+    printfn "*** End of Piping, Composition, Currying demo ***"
